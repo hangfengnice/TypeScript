e412ce6848220d97e779b9de3a75edf60b992c4a
hangfengnice <nicehangfeng@foxmail.com>
2019-07-18 20:02:43 +0800

    typescript

---
+++ greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+  return "Hello, " + person;
+}
+
+let user = 'hangfeng yingying jiaying';
+
+console.log(greeter(user))
---
+++ js/greeter.js
@@ -0,0 +1,6 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = 'hangfeng yingying jiaying';
+console.log(greeter(user));
