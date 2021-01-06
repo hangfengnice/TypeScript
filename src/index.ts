a1228066e9b3ea33d3117bc733ce49a9a1f56321
hangfeng <nicehangfeng@foxmail.com>
2021-01-06 09:03:30 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-declare type MethodDecorator = <T>(
-  target: Object,
-  propertyKey: string | symbol,
-  descriptor: TypedPropertyDescriptor<T>
-) => TypedPropertyDescriptor<T> | void;
+import _ from 'lodash'
+// _.add()
+
+
+let s: string | undefined
+
+console.log(s)
