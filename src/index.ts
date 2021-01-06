ec7c5da5ee97e9e3800fa14d56704ecac9ffb3b2
hangfeng <nicehangfeng@foxmail.com>
2021-01-06 20:42:36 +0800

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,46 @@
-import _ from 'lodash'
-// _.add()
 
+// function setName() {
+//   console.log('get setNmae')
+//   return (target) => {
+//     console.log('setName')
+//   }
+// }
+// function setName1() {
+//   console.log('get setNmae')
+//   return (target) => {
+//     console.log('setName')
+//   }
+// }
+// function setAge1() {
+//   console.log('get setAge')
+//   return (target) => {
+//     console.log('setAge')
+//   }
+// }
 
-let s: string | undefined
+// // function setAge() {
+// //   console.log('get setAge')
+// //   return () => {
+// //     console.log('setAge')
+// //   }
+// // }
 
-console.log(s)
+// @setAge1()
+// @setName()
+// class Foo {
+
+// }
+
+function required(target: any, propertyName: string, index: number) {
+  console.log(target, propertyName, index)
+}
+
+class A {
+  name = 'lision'
+  getInfo(@required prefix: string) {
+    return prefix
+  }
+}
+
+let a = new A()
+a.getInfo('helo')
